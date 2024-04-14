c633fa4c50aed56ca63c0c1cd7556b176c818250
Nem T. Lea <nemtielea@gmail.com>
2024-04-14 17:12:17 +0200

    dodanie programu mnozacego

---
+++ mnozenie.py
@@ -0,0 +1,17 @@
+# Wartosci stale
+
+SZABLON_POWITANIA = """Witaj, {}! Jestem kalkulatorem, w tej wersji potrafie tylko dodawac.
+Podaj dwie liczby, a ja podam ich sume."""
+
+# Przetwarzanie danych
+
+imie = input("Podaj swoje imie: ")
+
+print(SZABLON_POWITANIA.format(imie))
+
+czynnik_1 = float(input("Czynnik 1: "))
+czynnik_2 = float(input("Czynnik 2: "))
+
+iloczyn = czynnik_1 * czynnik_2
+
+print(f"Iloczyn liczb {czynnik_1} i {czynnik_2} to {iloczyn}")
