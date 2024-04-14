3016419a14a328ae6a645913e3d6486ba3cbee6e
Nem T. Lea <nemtielea@gmail.com>
2024-04-14 17:49:28 +0200

    dodanie nowej funkcji sprawdzania czy liczba jest mniejsza od zera

--- mnozenie.py
+++ mnozenie.py
@@ -1,7 +1,8 @@
 # Wartosci stale
 
 SZABLON_POWITANIA = """Witaj, {}! Jestem kalkulatorem, w tej wersji potrafie tylko mnozyc.
-Podaj dwie liczby, a ja podam ich iloczyn."""
+Podaj dwie liczby, a ja podam ich iloczyn.
+Powiem tez, czy ktoras z liczb jest mniejsza od 0."""
 
 # Przetwarzanie danych
 
@@ -13,5 +14,7 @@ czynnik_1 = float(input("Czynnik 1: "))
 czynnik_2 = float(input("Czynnik 2: "))
 
 iloczyn = czynnik_1 * czynnik_2
+jedna_z_liczb_mniejsza_od_0 = (czynnik_1 < 0) or (czynnik_2 < 0)
 
 print(f"Iloczyn liczb {czynnik_1} i {czynnik_2} to {iloczyn}")
+print(f"Jedna z liczb mniejsza od zera: {jedna_z_liczb_mniejsza_od_0}")
