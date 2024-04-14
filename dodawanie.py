0af38f8968305abbe9aaa4395fd2098c64925fad
Nem T. Lea <nemtielea@gmail.com>
2024-04-14 17:49:51 +0200

    dodanie nowej funkcji sprawdzajacej czy suma jest wieksza od zera

--- dodawanie.py
+++ dodawanie.py
@@ -1,6 +1,8 @@
 # Wartosci stale
+
 SZABLON_POWITANIA = """Witaj, {}! Jestem kalkulatorem, w tej wersji potrafie tylko dodawac.
-Podaj dwie liczby, a ja podam ich sume."""
+Podaj dwie liczby, a ja podam ich sume.
+Na koniec powiem tez, czy suma jest wieksza od 0."""
 
 # Przetwarzanie danych
 
@@ -12,5 +14,7 @@ liczba_1 = float(input("Liczba 1: "))
 liczba_2 = float(input("Liczba 2: "))
 
 suma = liczba_1 + liczba_2
+suma_wieksza_od_0 = suma > 0
 
 print(f"Suma liczb {liczba_1} i {liczba_2} to: {suma}")
+print(f"Suma wieksza od zera: {suma_wieksza_od_0}")
